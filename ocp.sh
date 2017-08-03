c419aabc343f56a99a68a7ce7930392ba4c48ffa
Mikelis Druvietis <mdruvietis@netsuite.com>
2017-08-03 22:33:09 +0200

    added comments

--- ocp.sh
+++ ocp.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# lets have date printed in beginning
 date
+# this will search your /etc/hosts
 grep -i $1 /etc/hosts | awk -F\  '{print $2}' | while read output
 do
+if [[ -z $output ]]; then echo "is what you are looking for in /etc/hosts?"; exit 1
+else
     ping -c 1 -i 0.2 -W 1.5  "$output" > /dev/null 
     if [ $? -eq 0 ]; then
     echo "node $output is up" 
     else
     echo "node $output is down"
+    fi
     fi
 done
